0ba23265ca084c239d68aebde18a11ae9a4cfae0
webdesignerkm <webdesignerkm@gmail.com>
2026-01-09 17:50:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ const ESP32_URL = location.origin;
 function toggleUI() {
     const mode = document.getElementById("mode").value;
     const timeBox = document.getElementById("timeBox");
+    const labelTemp = document.getElementById("labelTemp");
+
     if (mode === "2") {
         timeBox.classList.remove("hidden");
+        labelTemp.innerText = "Alap hőfok (időzítésen kívül)";
     } else {
         timeBox.classList.add("hidden");
+        labelTemp.innerText = "Kívánt hőfok (°C)";
     }
 }
 
@@ -17,16 +21,17 @@ function saveSettings() {
     let query = `/set?temp=${temp}&mode=${mode}`;
     
     if (mode === "2") {
+        const tempTimed = document.getElementById("targetInputTimed").value;
         const sh = document.getElementById("sh").value || 0;
         const sm = document.getElementById("sm").value || 0;
         const eh = document.getElementById("eh").value || 0;
         const em = document.getElementById("em").value || 0;
-        query += `&sh=${sh}&sm=${sm}&eh=${eh}&em=${em}`;
+        query += `&tempTimed=${tempTimed}&sh=${sh}&sm=${sm}&eh=${eh}&em=${em}`;
     }
 
     fetch(ESP32_URL + query)
-        .then(res => { if (res.ok) alert("Beállítások elmentve!"); })
-        .catch(err => alert("Hiba történt: " + err));
+        .then(res => { if (res.ok) alert("Sikeres mentés!"); })
+        .catch(err => alert("Hiba: " + err));
 }
 
 function loadData() {
@@ -37,9 +42,13 @@ function loadData() {
             document.getElementById("time").innerText = data.time;
             document.getElementById("targetDisp").innerText = data.target;
 
+            // Csak akkor írjuk felül az input mezőket, ha a felhasználó nem épp gépel bennük
             if (document.activeElement.tagName !== "INPUT" && document.activeElement.tagName !== "SELECT") {
-                document.getElementById("targetInput").value = data.target;
+                document.getElementById("targetInput").value = data.baseTarget;
                 document.getElementById("mode").value = data.mode;
+                if (data.mode == 2) {
+                    document.getElementById("targetInputTimed").value = data.timedTarget;
+                }
                 toggleUI();
             }
 
@@ -54,5 +63,5 @@ function loadData() {
         });
 }
 
-setInterval(loadData, 1000);
+setInterval(loadData, 2000);
 window.onload = loadData;
